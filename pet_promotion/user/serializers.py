abfcbc9d8c31fa8f49cb344ac8d9b13cc0eee9d7
Mactto <shc9928@gmail.com>
2021-03-02 01:45:16 +0900

    Login Token Added

--- pet_promotion/user/serializers.py
+++ pet_promotion/user/serializers.py
@@ -3,14 +3,10 @@ from rest_framework.validators import UniqueTogetherValidator
 from .models import User, Pet
 from board.models import Post, Comment
 
-'''
-class ProfileSerializer(serializers.ModelSerializer):
+class LoginSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = (
-            'id',
-            'name',
             'email',
             'password',
         )
-'''

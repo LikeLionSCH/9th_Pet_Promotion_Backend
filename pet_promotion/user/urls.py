05d8797eaf37c8387c5673dd20b1f50e7d718c02
Mactto <shc9928@gmail.com>
2021-03-02 16:28:43 +0900

    requirements update

--- pet_promotion/user/urls.py
+++ pet_promotion/user/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('login', LoginView.as_view(), name="login"),
     path('', UserView.as_view(), name="user"),
+    path('login', LoginView.as_view(), name="login"),
 ]
